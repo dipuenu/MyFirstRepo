02b192c09e0b2a9d8a787aafb3b11cb3f063d557
dipu <enudipu2202@gmail.com>
2020-07-01 15:25:01 +0530

    First branch added

---
+++ HelloGit/git/src/git/Sum.java
@@ -0,0 +1,17 @@
+package git;
+import java.util.Scanner;
+public class Sum {
+
+	public static void main(String[] args) {
+		
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter the two number");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int sum=0;
+		sum=a+b;
+		System.out.println("Sum is"+ sum);
+		
+	}
+
+}
